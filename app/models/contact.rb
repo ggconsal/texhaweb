4afc8e7f99fd4fcaedd1a243308e97a6e1d265fd
ggconsal <gconsal@gmail.com>
2015-08-12 15:01:56 -0700

    arreglos generales de cosmética. El blog esta al 80%. Resta upload de imágenes, revisión general de las CTAs y las páginas Home, quienes somos y Productos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,10 @@ class Contact < ActiveRecord::Base
   belongs_to :option
   belongs_to :canalingreso
 
+  has_many :post_comment_answers
+
+  accepts_nested_attributes_for :post_comment_answers
+
   has_secure_password
 
 end
